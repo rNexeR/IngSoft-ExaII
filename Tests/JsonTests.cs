6c0e902003c1454f5e275b5c07e2a4bf262e049c
kenystev <kjestevez.0795@gmail.com>
2017-03-24 16:39:32 -0600

    feature of json converter

--- Tests/JsonTests.cs
+++ Tests/JsonTests.cs
@@ -1,28 +1,30 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
 
 namespace Tests
 {
     [TestClass]
     public class JsonTests
     {
-        [TestMethod]
-        public void CheckSameColumns()
+        [Given(@"the next table representation of csv file")]
+        public void GivenTheNextTableRepresentationOfCsvFile(Table table)
         {
+            ScenarioContext.Current.Pending();
         }
 
-        [TestMethod]
-        public void IntegerIsParsedAsInteger()
+        [When(@"I press convert to Json")]
+        public void WhenIPressConvertToJson()
         {
+            ScenarioContext.Current.Pending();
         }
 
-        [TestMethod]
-        public void DateIsParsedAsDate()
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(string p0)
         {
+            Console.WriteLine(p0);
+            ScenarioContext.Current.Pending();
         }
 
-        [TestMethod]
-        public void StringIsParsedAsString()
-        {
-        }
     }
 }
