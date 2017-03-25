98a05c61a9fdcd9e16a47a31b0d9035e1af7e27a
NexeRodriguez <rnexer@gmail.com>
2017-03-24 20:28:21 -0600

    CsvParser class definition and tests

---
+++ Logic/Exceptions/HeaderNameCannotContainSpacesException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logic.Exceptions
+{
+    public class HeaderNameCannotContainSpacesException : Exception
+    {
+        public HeaderNameCannotContainSpacesException()
+        {
+        }
+
+        public HeaderNameCannotContainSpacesException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Tests/CsvParserTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using Logic.Converter;
+using Logic.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CsvParserTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(MoreColumnsThanHeadersException))]
+        public void TestMoreColumnsThanHeaders()
+        {
+            var dataArray = new[]
+               {
+                "nombre,apellido,edad",
+                "nexer,rodriguez,21,1804",
+                "kevin,estevez,20",
+                "tonio,mejia,21",
+                "josue,barahona,21,1804"
+            };
+
+            var data = $"{string.Join("\n", dataArray)}";
+
+            var parser = new CsvParser();
+            var csv = parser.Parse(data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LessColumnsThanHeadersException))]
+        public void TestLessColumnsThanHeaders()
+        {
+            var dataArray = new[]
+                {
+                "nombre,apellido,edad,depto",
+                "nexer,rodriguez,21",
+                "kevin,estevez,20",
+                "tonio,mejia,21",
+                "josue,barahona,21"
+            };
+
+            var data = $"{string.Join("\n", dataArray)}";
+
+            var parser = new CsvParser();
+            var csv = parser.Parse(data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HeaderNameCannotContainSpacesException))]
+        public void TestHeaderNameContainingSpaces()
+        {
+            var dataArray = new[]
+                {
+                "nombre de cliente,apellido,edad,depto",
+                "nexer,rodriguez,21",
+                "kevin,estevez,20",
+                "tonio,mejia,21",
+                "josue,barahona,21"
+            };
+
+            var data = $"{string.Join("\n", dataArray)}";
+
+            var parser = new CsvParser();
+            var csv = parser.Parse(data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HeaderNameCannotBeBlankException))]
+        public void TestBlankHeaderName()
+        {
+            var dataArray = new[]
+                {
+                ",apellido,edad,depto",
+                "nexer,rodriguez,21",
+                "kevin,estevez,20",
+                "tonio,mejia,21",
+                "josue,barahona,21"
+            };
+
+            var data = $"{string.Join("\n", dataArray)}";
+
+            var parser = new CsvParser();
+            var csv = parser.Parse(data);
+        }
+
+        [TestMethod]
+        public void TestCorrectParse()
+        {
+            var dataArray = new[]
+            {
+                "nombre,apellido,edad",
+                "nexer,rodriguez,21",
+                "kevin,estevez,20",
+                "tonio,mejia,21",
+                "josue,barahona,21"
+            };
+
+            var data = $"{string.Join("\n", dataArray)}";
+
+            var parser = new CsvParser();
+            var csv = parser.Parse(data);
+            Assert.IsTrue(csv.Headers.Count == 3);
+        }
+    }
+}
